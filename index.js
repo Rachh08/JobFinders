c9d8835996364922a1629f71d8765196b3f993b4
Rachh08 <2200667J@student.tp.edu.sg>
2023-11-15 23:50:48 +0800

    Created the base Node.js project

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
+const { Filter } = require('./utils/FilterUtil')
+app.get('/Filter', Filter);
+
 app.get('/', (req, res) => {
 res.sendFile(__dirname + "/public/" + startPage);
 })
---
+++ utils/FilterUtil.js
@@ -0,0 +1,7 @@
+//filter items based on a condition
+function filterByCondition(items, conditionFunction) {
+    return items.filter(conditionFunction);
+  }
+  
+  module.exports = { filterByCondition };
+  

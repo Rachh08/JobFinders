589e98a9c8ed9b563a9c2f07e459af0acbdcb35d
Rachh08 <2200667J@student.tp.edu.sg>
2023-11-25 13:04:44 +0800

    added comment for latest code

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ class User {
         this.password = password;
         this.mobile = mobile;
 
+        //Creating Unique Id for each user
         const timestamp = new Date().getTime();
         const random = Math.floor(Math.random() * 1000);
         this.id = timestamp + "" + random.toString().padStart(3, '0');

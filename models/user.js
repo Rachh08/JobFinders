913de61eea45c6f0ab59859df7d8bee7e5098d9c
Rachh08 <2200667J@student.tp.edu.sg>
2023-11-25 15:21:37 +0800

    Error Handling

--- models/user.js
+++ models/user.js
@@ -4,12 +4,6 @@ class User {
     this.email = email;
     this.password = password;
     this.mobile = mobile;
-
-    //Creating Unique Id for each user
-    const timestamp = new Date().getTime();
-    const random = Math.floor(Math.random() * 1000);
-    this.id = timestamp + "" + random.toString().padStart(3, '0');
-    
     }
     }
     module.exports = { User };

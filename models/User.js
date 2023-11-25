914931a7ad278329d821acdea6579623b04cae39
Rachh08 <148046594+Rachh08@users.noreply.github.com>
2023-11-25 16:04:51 +0800

    Update User.js
    Deleting this File

--- models/User.js
+++ models/User.js
@@ -1,9 +1,1 @@
-class User {
-    constructor(name, email, password, mobile) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.mobile = mobile;
-    }
-    }
-    module.exports = { User };
+

b142de36e218bab9ce8bf3b519d9af4e5fa26e62
Rachh08 <2200667J@student.tp.edu.sg>
2023-11-25 15:46:50 +0800

    implemented code to create unique user id

--- models/user.js
+++ models/user.js
@@ -4,6 +4,12 @@ class User {
     this.email = email;
     this.password = password;
     this.mobile = mobile;
+
+    //Creating Unique Id for each user
+    const timestamp = new Date().getTime();
+    const random = Math.floor(Math.random() * 1000);
+    this.id = timestamp + "" + random.toString().padStart(3, '0');
+    
     }
     }
     module.exports = { User };

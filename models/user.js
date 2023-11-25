5fb200276918d4ab17a0ea02632e2eade4265850
Rachh08 <2200667J@student.tp.edu.sg>
2023-11-25 15:41:58 +0800

    Error handling

--- models/user.js
+++ models/user.js
@@ -1,15 +1,9 @@
 class User {
     constructor(name, email, password, mobile) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.mobile = mobile;
-
-        //Creating Unique Id for each user
-        const timestamp = new Date().getTime();
-        const random = Math.floor(Math.random() * 1000);
-        this.id = timestamp + "" + random.toString().padStart(3, '0');
-
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.mobile = mobile;
     }
-}
-module.exports = { User };
+    }
+    module.exports = { User };

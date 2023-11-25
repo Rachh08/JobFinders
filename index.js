df2d3612604eb47674f19f5a9d661e3777eee730
AnraSjar <141249570+AnraSjar@users.noreply.github.com>
2023-11-25 12:05:03 +0800

    Update index.js to include the routes

--- index.js
+++ index.js
@@ -9,11 +9,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
+const { JobFilter } = require('./utils/JobSearchUtil')
+app.get('/filterJobs', JobFilter);
 
+const { register } = require('./utils/UserUtil');
+const { JobSearch } = require('./models/JobSearch');
+app.post('/register', register);
+
+app.get('/search', (req, res) => { 
+    const query = req.query.q; 
+    const results = JobSearch(query); 
+    res.json({ results });
+});
+
+const { login } = require('./utils/UserUtil')
+app.post('/login', login);
 
 const { viewJobs } = require('./utils/JobsUtil')
 app.get('/view-jobs', viewJobs);
 
+const { updateUserdetails } = require('./utils/UserUtil')
+app.post('/update-userdetails', updateUserdetails);
 
 app.get('/', (req, res) => {
 res.sendFile(__dirname + "/public/" + startPage);

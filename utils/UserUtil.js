551788baddfda19dd3771532c71b831849374d19
Rachh08 <2200667J@student.tp.edu.sg>
2023-11-26 21:46:53 +0800

    added more error messages to code

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -21,18 +21,40 @@ async function register(req, res) {
         const name = req.body.name;
         const mobile = req.body.mobile;
 
-// Checks if the email string does not include the '@'and '.com' substring, checks if the length of the password string is less than 8 characters
-// Checks if the password string contains at least one uppercase letter and one special character, checks if the name string contains only letters
-// Checks if the mobile string is not an empty string or does not contain only whitespace, checks if the mobile string contains exactly 8 digits
-
-        if (!email.includes('@') || !email.includes('.com') || password.length < 8 ||!/(?=.*[A-Z])(?=.*[!@#$%^&*])/.test(password)
-        ||!/^[a-zA-Z]+$/.test(name) ||!mobile.trim()|| !/^\d{8}$/.test(mobile)) {
-            return res.status(500).json({ message: 'Validation error' });
-        } else {
-            const newUser = new User(email, password, name, mobile);
-            const updatedUsers = await writeJSON(newUser, 'utils/users.json');
-            return res.status(201).json(updatedUsers);
+        // Checks if all fields are filled in 
+        if (!email || !password || !name || !mobile) {
+            return res.status(400).json({ message: 'All fields are required for registration.' });
         }
+        // Checks if the email string does not include the '@'and '.com' substring
+        if (!email.includes('@') || !email.includes('.com')) {
+            return res.status(500).json({ message: 'Invalid email format.' });
+        }
+
+        // Checks if the length of the password string is less than 8 characters
+        if (password.length < 8) {
+            return res.status(500).json({ message: 'Password must be at least 8 characters long.' });
+        }
+
+        // Checks if the password string contains at least one uppercase letter 
+        if (!/(?=.*[A-Z])/.test(password)) {
+            return res.status(500).json({ message: 'Password must contain at least one uppercase letter.' });
+        }
+        // Checks if the password string contains one special character
+        if (!/(?=.*[!@#$%^&*])/.test(password)) {
+            return res.status(500).json({ message: 'Password must contain at least one special character.' });
+        }
+        // Checks if the name string contains only letters and is filled in
+        if (!/^[a-zA-Z]+$/.test(name) || name.length === 0) {
+            return res.status(500).json({ message: 'Invalid name format.' });
+        }        
+        // Checks if the mobile string contains exactly 8 digits
+        if (!mobile.trim() || !/^\d{8}$/.test(mobile)) {
+            return res.status(500).json({ message: 'Invalid mobile format.' });
+        }
+
+        const newUser = new User(email, password, name, mobile);
+        const updatedUsers = await writeJSON(newUser, 'utils/users.json');
+        return res.status(201).json(updatedUsers);
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }

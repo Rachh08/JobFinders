24a3d3fa7e669b32fbecc778e632f3a1c5c3941c
DarrianLee <2202666j@student.tp.edu.sg>
2023-11-26 13:15:57 +0800

    add conditions for update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(express.static("./public"));
 
 const { updateUserdetails } = require('./utils/UserUtil')
-app.put('/update-userdetails/:id', updateUserdetails);
+app.put('/update-user/:id', updateUserdetails);
 
 app.get('/', (req, res) => {
 res.sendFile(__dirname + "/public/" + startPage);
--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -20,36 +20,60 @@ async function writeJSON(object, filename) {
 
 async function updateUserdetails(req, res) {
     try {
-    const id = req.parmas.id;
+    const id = req.params.id;
     const password = req.body.password;
     const mobile = req.body.mobile;
     
     const allUsers = await readJSON('utils/users.json');
 
-    var update = false;
+        var edit = false;
 
-    for (var i = 0; i < allUsers.length; i++) {
-        var currentUser = allUsers[i];
-        if (currentUser.id == id){
-            allUsers[i].password = password;
-            allUsers[i].mobile = mobile;
+        for (var i = 0; i < allUsers.length; i++) {
+            var currUser = allUsers[i];
+            if (currUser.id == id){
+                allUsers[i].password = password;
+                allUsers[i].mobile = mobile;
 
-            update = true;
+                edit = true;
+            }   
+        }
+        //to update the password and mobile 
+       if (edit) {
+            await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+           return res.status(201).json({ message: 'User details has been successfully updated!' });
+      }  //if password exceeds 8 digits
+        if ( password.length > 8){
+            await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+            return res.status(500).json({message: "Password should not have more than 8 digits!"});
+        }  //if password does not consist of uppercase  
+        if ( !/(?=.*[A-Za-z])(?=.*[!@#$%^&*])/.test(password)) {
+            await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+            return res.status(500).json({message: "Password should contain one upper case letter and special character and must not have numbers!"})
         }   
+        
+        // if mobile number consist of letter 
+        if ( !/^[0-9]/.test(mobile)) {
+           await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+           return res.status(500).json({message: "Mobile number should contain only numbers!"});
+        }
+        //if  mobile number exceeds 8 digits
+        if ( mobile.length > 8){
+            await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+            return res.status(500).json({message: "Mobile Number should not have more than 8 digits "});
+        }  
+         //if user does not fill all input 
+        if (!mobile || !password) {
+            await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
+            return res.status(400).json({ message: 'All input fields must be filled!' });
+        } 
+        else {
+            return res.status(201).json({ message: 'User details is unsuccessful!' });
+        }
     }
-
-    if (update) {
-        await fs.writeFile('utils/users.json', JSON.stringify(allUsers), 'utf8');
-        return res.status(201).json({ message: 'User details updated successfully!' });
-    } else {
-        return res.status(500).json({ message: 'Unable to update user details!' });
-    }
-    } catch (error) {
+    catch (error) {
         return res.status(500).json({ message: error.message });
     }
-
 }
-
 module.exports = {
 readJSON, writeJSON, updateUserdetails
 };

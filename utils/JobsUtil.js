83d9ef2357492eee3157be0711837cab615884f6
AnraSjar <tacendicanne@gmail.com>
2023-11-26 12:12:30 +0800

    Implemented Add-Jobs features- final

--- utils/JobsUtil.js
+++ utils/JobsUtil.js
@@ -4,18 +4,38 @@ const { Job } = require('../models/jobs.js');
 
 async function addJobs(req, res) {
     try {
-    const name = req.body.name;
-    const company = req.body.company;
-    const location = req.body.location;
-    const description = req.body.description;
-    const contact = req.body.contact;
-    const newJob = new Job(name, company, location, description, contact);
-    const updatedJob = await writeJSON(newJob, 'utils/jobs.json');
-    return res.status(201).json(updatedJob);
+        const jobName = req.body.jobName;
+        const company = req.body.company;
+        const location = req.body.location;
+        const description = req.body.description;
+        const contact = req.body.contact;
+
+        // Check if any of the input fields is empty
+        if (!jobName || !company || !location || !description || !contact) {
+            return res.status(400).json({ message: 'All input fields must be filled.' });
+        }
+
+
+         // Validate jobName and company fields
+         const onlyAlpha= /^[A-Za-z\s]{1,50}$/; // Alphabets and spaces, up to 50 characters
+         if (!onlyAlpha.test(jobName) || !onlyAlpha.test(company)) {
+             return res.status(400).json({ message: 'Job name and company must contain alphabets only and be up to 50 characters long.' });
+         }
+
+         //Validate contact to include only 1 @ so that user provides contact email
+         const oneSpecial = /^[^@]*@[^@]+$/;
+         if(!oneSpecial.test(contact)) {
+            return res.status(400).json({message: "Provide valid contact details: email"})
+         } 
+
+        const newJob = new Job(jobName, company, location, description, contact);
+
+        const updatedJob = await writeJSON(newJob, 'utils/jobs.json');
+        return res.status(201).json(updatedJob);
     } catch (error) {
-    return res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
-    }
+}
 
 
 async function viewJobs(req, res) {
@@ -28,5 +48,5 @@ async function viewJobs(req, res) {
 }
 
 module.exports = {
-    addJobs,viewJobs
+    addJobs, viewJobs
 };

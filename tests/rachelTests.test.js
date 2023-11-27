ef5896936e746512c98f3cf67e3ec8f8452893bf
Rachh08 <2200667J@student.tp.edu.sg>
2023-11-27 20:13:23 +0800

    modified backend code

--- tests/rachelTests.test.js
+++ tests/rachelTests.test.js
@@ -36,7 +36,7 @@ describe('Testing Register Function', () => {
                 mobile: '87821800',
             },
         };
-    
+
         const res = {
             status: function (code) {
                 expect(code).to.equal(201);
@@ -53,82 +53,83 @@ describe('Testing Register Function', () => {
                 }
             },
         };
-    
+
         // Call your register function
         await register(req, res);
     });
-    
 
-describe('Testing searchJobs Function', () => {
-    it('Should return matching jobs for a valid query', async () => {
-        const validQuery = 'developer'; 
-        // Provide a valid search query
-        const req = {
-            body: {
-                q: validQuery,
-            },
-        };
 
-        const res = {
-            status: function (code) {
-                // Expecting a successful response code
-                expect(code).to.equal(200);
-                return this;
-            },
-            json: function (data) {
-                // Expecting data to be an array (matching jobs)
-                expect(data).to.be.an('array');
-            },
-        };
+    describe('Testing searchJobs Function', () => {
+        it('Should return matching jobs for a valid query', async () => {
+            const validQuery = 'developer';
+            // Provide a valid search query
+            const req = {
+                body: {
+                    q: validQuery,
+                },
+            };
 
-        // Call searchJobs function
-        await searchJobs(req, res);
+            const res = {
+                status: function (code) {
+                    // Expecting a successful response code
+                    expect(code).to.equal(200);
+                    return this;
+                },
+                json: function (data) {
+                    // Expecting data to be an array (matching jobs)
+                    expect(data).to.be.an('array');
+                },
+            };
+
+            // Call searchJobs function
+            await searchJobs(req, res);
+        });
+
+        it('Should return 404 for no matching jobs', async () => {
+            const invalidQuery = 'invalidquery'; // Provide a query with no matching jobs
+            const req = {
+                body: {
+                    q: invalidQuery,
+                },
+            };
+
+            const res = {
+                status: function (code) {
+                    // Expecting a not found response code
+                    expect(code).to.equal(404);
+                    return this;
+                },
+                json: function (data) {
+                    // Expecting a message about no matching jobs
+                    expect(data).to.have.property('message').to.equal('No matching jobs found.');
+                },
+            };
+
+            await searchJobs(req, res);
+        });
+
+        it('Should return 400 for an invalid search query', async () => {
+            const invalidQuery = '123invalid'; // Provide an invalid query (contains numbers)
+            const req = {
+                body: {
+                    q: invalidQuery,
+                },
+            };
+
+            const res = {
+                status: function (code) {
+                    // Expecting a bad request response code
+                    expect(code).to.equal(400);
+                    return this;
+                },
+                json: function (data) {
+                    // Expecting a message about an invalid search query
+                    expect(data).to.have.property('message').to.equal('Search query should only contain letters.');
+                },
+            };
+
+            // Call your searchJobs function
+            await searchJobs(req, res);
+        });
     });
-
-    it('Should return 404 for no matching jobs', async () => {
-        const invalidQuery = 'invalidquery'; // Provide a query with no matching jobs
-        const req = {
-            body: {
-                q: invalidQuery,
-            },
-        };
-
-        const res = {
-            status: function (code) {
-                // Expecting a not found response code
-                expect(code).to.equal(404);
-                return this;
-            },
-            json: function (data) {
-                // Expecting a message about no matching jobs
-                expect(data).to.have.property('message').to.equal('No matching jobs found.');
-            },
-        };
-
-        await searchJobs(req, res);
-    });
-
-    it('Should return 400 for an invalid search query', async () => {
-        const invalidQuery = '123invalid'; // Provide an invalid query (contains numbers)
-        const req = {
-            body: {
-                q: invalidQuery,
-            },
-        };
-
-        const res = {
-            status: function (code) {
-                // Expecting a bad request response code
-                expect(code).to.equal(400);
-                return this;
-            },
-            json: function (data) {
-                // Expecting a message about an invalid search query
-                expect(data).to.have.property('message').to.equal('Search query should only contain letters.');
-            },
-        };
-
-        // Call your searchJobs function
-        await searchJobs(req, res);
-    });
-});
+})

54e7fdbd0c2836849bda61f0e6d6ea0267bd269b
AnraSjar <tacendicanne@gmail.com>
2023-11-28 21:23:50 +0800

    Implemented Unit Testing

--- tests/annTests.test.js
+++ tests/annTests.test.js
@@ -5,6 +5,51 @@ const fs = require('fs').promises;
 const { readJSON, deleteUser, writeJSON } = require('../utils/UserUtil.js')
 const { viewJobs, addJobs } = require('../utils/JobsUtil.js')
 
+describe("Testing View Job Functions",  () => {
+    //const jobFilePath = '../utils/jobs.json';
+
+    const expectedData = [
+        {
+            "jobName": "Assistant Chef",
+            "company": "Gordon Ramsay Kitchen",
+            "location": "Bedok Green",
+            "description": "Cook delicious food and work under renowned chef Gordon Ramsay. Training Provided. Send portfolio with resume. Thanks",
+            "contact": "littlechef@gmail.com",
+            "id": "11242023001"
+        },
+        {
+            "jobName": "Web Developer Intern",
+            "company": "Web Company",
+            "location": "Jurong West",
+            "description": "Recruiting Interns for front-end development of mobile app. Requirement: Good UI and UX skills.",
+            "contact": "123john@gmail.com, 0471-2345",
+            "id": "11242023002"
+        }
+    ];
+    
+
+    const req = {};
+    const res = {
+        status: (code) => ({ json: (data) => ({ code, data }) }),
+    };
+
+    
+
+    it("Should view all Jobs successfully", async () => {
+        const result = await viewJobs(res, req);
+
+        expect(result.code).to.equal(201);
+        expect(result).to.deep.equal({
+
+            code: 201,
+            data:expectedData
+
+
+        })
+
+    });
+});
+
 
 describe('Testing Add Job Functions', () => {
     const jobFilePath = 'utils/jobs.json';
@@ -112,102 +157,82 @@ describe('Testing Add Job Functions', () => {
 });
 
 describe('Delete User Function Testing', () => {
-        // Helper function to create a mock user for testing
-        const createMockUser = (name, password) => ({ name, password });
-      
-        // Helper function to create a mock request with parameters
-        const createMockRequest = (name, password) => ({
-          params: { name },
-          body: { password },
-        });
-      
-        // Helper function to create a mock response
-        const createMockResponse = () => {
-          const res = {
+    // Helper function to create a mock user for testing
+    const createMockUser = (name, password) => ({ name, password });
+
+    // Helper function to create a mock request with parameters
+    const createMockRequest = (name, password) => ({
+        params: { name },
+        body: { password },
+    });
+
+    // Helper function to create a mock response
+    const createMockResponse = () => {
+        const res = {
             status: sinon.stub().returnsThis(),
             json: sinon.stub(),
-          };
-          return res;
         };
-      
-        it('should delete user successfully', async () => {
-          // Arrange
-          const mockUser = createMockUser('testUser', 'testPassword');
-          const mockRequest = createMockRequest('testUser', 'testPassword');
-          const mockResponse = createMockResponse();
-      
-          // Mock readJSON to return a list with the mock user
-          sinon.stub(fs, 'readFile').resolves(JSON.stringify([mockUser]));
-      
-          // Act
-          await deleteUser(mockRequest, mockResponse);
-      
-          // Assert
-          sinon.assert.calledWith(mockResponse.status, 200);
-          sinon.assert.calledWith(mockResponse.json, { message: `User 'testUser' deleted successfully.` });
-      
-          // Cleanup
-          sinon.restore();
-        });
-      
-        it('should handle incorrect password', async () => {
-          // Arrange
-          const mockUser = createMockUser('testUser', 'testPassword');
-          const mockRequest = createMockRequest('testUser', 'wrongPassword');
-          const mockResponse = createMockResponse();
-      
-          // Mock readJSON to return a list with the mock user
-          sinon.stub(fs, 'readFile').resolves(JSON.stringify([mockUser]));
-      
-          // Act
-          await deleteUser(mockRequest, mockResponse);
-      
-          // Assert
-          sinon.assert.calledWith(mockResponse.status, 401);
-          sinon.assert.calledWith(mockResponse.json, { message: 'Incorrect password. Deletion failed.' });
-      
-          // Cleanup
-          sinon.restore();
-        });
-      
-        it('should handle user not found', async () => {
-          // Arrange
-          const mockRequest = createMockRequest('nonexistentUser', 'password');
-          const mockResponse = createMockResponse();
-      
-          // Mock readJSON to return an empty list
-          sinon.stub(fs, 'readFile').resolves('[]');
-      
-          // Act
-          await deleteUser(mockRequest, mockResponse);
-      
-          // Assert
-          sinon.assert.calledWith(mockResponse.status, 404);
-          sinon.assert.calledWith(mockResponse.json, { message: 'User \'nonexistentUser\' not found.' });
-      
-          // Cleanup
-          sinon.restore();
-        });
-      
-        it('should handle internal server error', async () => {
-          // Arrange
-          const mockRequest = createMockRequest('testUser', 'testPassword');
-          const mockResponse = createMockResponse();
-      
-          // Mock readJSON to throw an error
-          sinon.stub(fs, 'readFile').rejects(new Error('Internal server error'));
-      
-          // Act
-          await deleteUser(mockRequest, mockResponse);
-      
-          // Assert
-          sinon.assert.calledWith(mockResponse.status, 500);
-          sinon.assert.calledWith(mockResponse.json, { message: 'Internal server error' });
-      
-          // Cleanup
-          sinon.restore();
-        });
-    
+        return res;
+    };
+
+    it('should delete user successfully', async () => {
+        // Arrange
+        const mockUser = createMockUser('testUser', 'testPassword');
+        const mockRequest = createMockRequest('testUser', 'testPassword');
+        const mockResponse = createMockResponse();
+
+        // Mock readJSON to return a list with the mock user
+        sinon.stub(fs, 'readFile').resolves(JSON.stringify([mockUser]));
+
+        // Act
+        await deleteUser(mockRequest, mockResponse);
+
+        // Assert
+        sinon.assert.calledWith(mockResponse.status, 200);
+        sinon.assert.calledWith(mockResponse.json, { message: `User 'testUser' deleted successfully.` });
+
+        // Cleanup
+        sinon.restore();
+    });
+
+    it('should handle incorrect password', async () => {
+        // Arrange
+        const mockUser = createMockUser('testUser', 'testPassword');
+        const mockRequest = createMockRequest('testUser', 'wrongPassword');
+        const mockResponse = createMockResponse();
+
+        // Mock readJSON to return a list with the mock user
+        sinon.stub(fs, 'readFile').resolves(JSON.stringify([mockUser]));
+
+        // Act
+        await deleteUser(mockRequest, mockResponse);
+
+        // Assert
+        sinon.assert.calledWith(mockResponse.status, 401);
+        sinon.assert.calledWith(mockResponse.json, { message: 'Incorrect password. Deletion failed.' });
+
+        // Cleanup
+        sinon.restore();
+    });
+
+    it('should handle user not found', async () => {
+        // Arrange
+        const mockRequest = createMockRequest('nonexistentUser', 'password');
+        const mockResponse = createMockResponse();
+
+        // Mock readJSON to return an empty list
+        sinon.stub(fs, 'readFile').resolves('[]');
+
+        // Act
+        await deleteUser(mockRequest, mockResponse);
+
+        // Assert
+        sinon.assert.calledWith(mockResponse.status, 404);
+        sinon.assert.calledWith(mockResponse.json, { message: 'User \'nonexistentUser\' not found.' });
+
+        // Cleanup
+        sinon.restore();
+    });
 
 });
 
